c1444786c6d87376878f637214979a1a34f42d7d
Luis Cetta <luiscetta@gmail.com>
2020-12-31 15:43:36 -0300

    creating frontend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({ 
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Filipe Cetta'
+    });
+});
+
+app.listen(3333);
